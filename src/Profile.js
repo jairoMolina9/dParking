fddfd8f3155fb105dea7ea1dc189c46207653952
edwincarbajal <exaviercarbajal@gmail.com>
2019-11-16 03:03:44 -0500

    Add time picker and address bar component

--- src/Profile.js
+++ src/Profile.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 import {
   Person,
 } from 'blockstack';
+import {
+  Drawer,
+  Button,
+  Card,
+  Row,
+  Col,
+  Input,
+  TimePicker
+} from 'antd';
+import moment from 'moment';
 
+const format = 'HH:mm';
 const avatarFallbackImage = 'https://s3.amazonaws.com/onename/avatar-placeholder.png';
 
 export default class Profile extends Component {
@@ -18,12 +29,29 @@ export default class Profile extends Component {
   	  	  return avatarFallbackImage;
   	  	},
   	  },
+      visible: false,
+      placement: 'bottom'
   	};
+    this.showDrawer = this.showDrawer.bind(this);
+    this.onClose = this.onClose.bind(this);
   }
+
+  showDrawer = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  onClose = () => {
+    this.setState({
+      visible: false,
+    });
+  };
 
   render() {
     const { handleSignOut, userSession } = this.props;
     const { person } = this.state;
+    const { Search } = Input;
     return (
       !userSession.isSignInPending() ?
       <div className="panel-welcome" id="section-2">
@@ -40,6 +68,36 @@ export default class Profile extends Component {
             Logout
           </button>
         </p>
+        <Row>
+          <Col span={24}>
+            <Card title={`Hello, ${person.name() ? person.name() : 'Nameless Person'}`} bordered={false}>
+              <Search
+                size="large"
+                placeholder="Enter Address"
+                onSearch={value => console.log(value)}
+              />
+              <br />
+              <br />
+              <br />
+              <button
+                className="btn btn-primary btn-lg"
+                id="signout-button"
+                onClick={this.showDrawer}
+              >
+                Confirm Address
+              </button>
+            </Card>
+          </Col>
+        </Row>
+        <Drawer
+          title="Select Duration"
+          placement={this.state.placement}
+          closable={false}
+          onClose={this.onClose}
+          visible={this.state.visible}
+        >
+          <TimePicker defaultValue={moment('00:00', format)} format={format} minuteStep={10} size="large" />
+        </Drawer>
       </div> : null
     );
   }
